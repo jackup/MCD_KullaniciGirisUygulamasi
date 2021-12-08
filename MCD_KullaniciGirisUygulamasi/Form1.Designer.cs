b0d2a9532a833543a0820499dc9e6338c924f602
jackup <yk.58@hotmail.com>
2021-12-08 16:30:58 +0300

    Add project files.

---
+++ MCD_KullaniciGirisUygulamasi/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace MCD_KullaniciGirisUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKullaniciAd = new System.Windows.Forms.Label();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.txtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblKullaniciAd
+            // 
+            this.lblKullaniciAd.AutoSize = true;
+            this.lblKullaniciAd.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullaniciAd.Location = new System.Drawing.Point(222, 99);
+            this.lblKullaniciAd.Name = "lblKullaniciAd";
+            this.lblKullaniciAd.Size = new System.Drawing.Size(135, 20);
+            this.lblKullaniciAd.TabIndex = 0;
+            this.lblKullaniciAd.Text = "Kullanıcı Adı:";
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSifre.Location = new System.Drawing.Point(222, 128);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(60, 20);
+            this.lblSifre.TabIndex = 1;
+            this.lblSifre.Text = "Şifre:";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGirisYap.Location = new System.Drawing.Point(391, 186);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(141, 33);
+            this.btnGirisYap.TabIndex = 2;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // txtKullaniciAd
+            // 
+            this.txtKullaniciAd.Location = new System.Drawing.Point(391, 99);
+            this.txtKullaniciAd.Name = "txtKullaniciAd";
+            this.txtKullaniciAd.Size = new System.Drawing.Size(141, 22);
+            this.txtKullaniciAd.TabIndex = 3;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(391, 127);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(141, 22);
+            this.txtSifre.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAd);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.lblKullaniciAd);
+            this.Name = "Form1";
+            this.Text = "Giriş Ekranı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKullaniciAd;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.TextBox txtKullaniciAd;
+        private System.Windows.Forms.TextBox txtSifre;
+    }
+}
+
